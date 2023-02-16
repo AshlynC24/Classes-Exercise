2c299ad010878333751fbc9ab4042d17c4e832a1
Ashlyn Carmosino <ashlyncarmosino@Ashlyns-Air.lan>
2023-02-16 14:22:12 -0500

    Exercise 1

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace Classes
+﻿using System;
+using System.Reflection;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+              public class Car
+        {
+            public string Make { get; set; }
+            public string Model { get; set; }
+            public int Year { get; set; }
+
+
         }
+        public void HowOldCarIs()
+        {
+            Console.WriteLine($"Year of {myCar.Make} {myCar.Model} is {myCar.Year}");
+        }
+        Car myCar = new Car()
+        {
+            Make = "Ford",
+            Model = "F-150",
+            Year = 2015,
+        };
+    }
+   
     }
 }
